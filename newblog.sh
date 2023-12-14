567b617515f9ecd01e5385a233eeaf5bd854af07
ziyang <iyuanzz@qq.com>
2023-12-14 23:18:10 +0800

    newblog

--- newblog.sh
+++ newblog.sh
@@ -4,7 +4,7 @@ file_name=./source/_drafts/$1.md
 echo "---" >> $file_name
 echo "title:" >> $file_name
 time=$(date "+%Y-%m-%d %H:%M:%S")
-echo "tags: 产品经理" >> $file_name
+echo "tags: " >> $file_name
 echo "categories: 杂谈" >> $file_name
 echo "date: $time" >> $file_name
 echo "description:" >> $file_name
